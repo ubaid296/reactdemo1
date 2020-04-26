0d088cadd808bd225134b257610d362e65ea1e86
aboubakarch1 <abou.bakar@enou.co>
2020-04-26 23:35:55 +0500

    parent child component state handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
   render() {
-    const handleDecrement = () => {
-      let count = this.state.count;
-      if (count > 0) count--;
-      this.setState({ count: count });
-    };
-    const handleIncrement = () => {
-      let count = this.state.count;
-      count++;
-      this.setState({ count: count });
-    };
     return (
-      <div className="row m-5">
+      <div key={this.props.data.id} className="row m-5">
         <button
           className="btn btn-counter btn-danger"
-          onClick={handleDecrement}
+          onClick={() => this.props.handleDecrement(this.props.data)}
         >
           -
         </button>
         <p style={{ marginLeft: 20, marginRight: 20, paddingTop: 5 }}>
-          {this.state.count}
+          {this.props.data.count}
         </p>
         <button
           className="btn btn-counter btn-primary"
-          onClick={handleIncrement}
+          onClick={() => this.props.handleIncrement(this.props.data)}
         >
           +
         </button>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,7 +3,39 @@ import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./counter";
 
 class Counters extends Component {
+  state = {
+    counter: [
+      { id: 1, count: 0 },
+      { id: 2, count: 0 },
+      { id: 3, count: 0 },
+      { id: 4, count: 0 },
+      { id: 5, count: 0 },
+    ],
+    itemCount: 0,
+  };
   render() {
+    const handleDecrement = (data) => {
+      let { counter, itemCount } = this.state;
+      const index = counter.indexOf(data);
+      if (counter[index].count > 0) {
+        counter[index].count--;
+        let count = counter[index].count;
+        if (count === 0) {
+          itemCount--;
+        }
+      }
+      this.setState({ itemCount: itemCount, counter: counter });
+    };
+    const handleIncrement = (data) => {
+      let { counter, itemCount } = this.state;
+      const index = counter.indexOf(data);
+      let count = counter[index].count;
+      if (count === 0) {
+        itemCount++;
+      }
+      counter[index].count++;
+      this.setState({ itemCount: itemCount, counter: counter });
+    };
     return (
       <div>
         <nav
@@ -11,8 +43,21 @@ class Counters extends Component {
           className="navbar "
         >
           <p style={{ paddingTop: 10 }}>Count</p>
+          <span
+            className="badge badge-pill badge-dark"
+            style={{ fontSize: 20 }}
+          >
+            {this.state.itemCount}
+          </span>
         </nav>
-        <Counter />
+        {this.state.counter.map((c) => (
+          <Counter
+            key={c.id}
+            data={c}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+          />
+        ))}
       </div>
     );
   }
